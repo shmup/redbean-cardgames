e8a2b781504c2f30a39faf352e176ad847a9cfdd
Jared Miller <jared@smell.flowers>
2022-08-20 11:49:32 -0400

    Add an index.html and some cute pointers

--- srv/.init.lua
+++ srv/.init.lua
@@ -5,26 +5,17 @@ fm.setRoute("/favicon.ico", fm.serveAsset("/static/suits.png"))
 fm.setRoute("/js/*", fm.serveAsset)
 fm.setRoute("/css/*", fm.serveAsset)
 fm.setRoute("/img/*", fm.serveAsset)
-fm.setRoute("/calculation", fm.serveAsset("/templates/calculation.html"))
+
+fm.setRoute("/", fm.serveAsset("/templates/index.html"))
 fm.setRoute("/cascades", fm.serveAsset("/templates/cascades.html"))
 
-fm.setRoute("/", function()
-	return [[
-      <p>Current WIP:</p>
-      <ul>
-        <li><a href="/calculation">Calculation</a> - Laying out the groundwork, not an actual game yet</li>
-      </ul>
-    ]]
-end)
-
+fm.setRoute("/calculation", fm.serveAsset("/templates/calculation.html"))
 fm.setRoute("/api/v1/calculation/init", function()
 	return fm.serveContent("json", calculation.init())
 end)
-
 fm.setRoute("/api/v1/calculation/talon", function()
 	return fm.serveContent("json", { id = 15 })
 end)
-
 fm.setRoute(fm.POST{ "/api/v1/calculation/pack_topcard/:t_id[%d]" }, function(r)
 	local tableau_id = tonumber(r.params.t_id)
 
